20ae2bbdd7aa8d358c229bfc99e4c6b557dc5ba4
swathi uppala <uppalaswathi@gmail.com>
2019-09-04 12:05:59 -0700

    Update ListItemtype.py

--- ListItemtype.py
+++ ListItemtype.py
@@ -1,3 +1,18 @@
+#Write a Python program that prints each item and its corresponding type from the following list.
+#Sample List : datalist = [1452, 11.23, 1+2j, True, 'w3resource', (0, -1), [5, 12], {"class":'V', "section":'A'}]
+
 datalist = [1452, 11.23, 1+2j, True, 'w3resource', (0, -1), [5, 12], {"class":'V', "section":'A'}]
 for item in datalist:
   print("type of", item ,"is", type(item))
+  
+"""  
+Output:
+type of 1452 is <class 'int'>
+type of 11.23 is <class 'float'>
+type of (1+2j) is <class 'complex'>
+type of True is <class 'bool'>
+type of w3resource is <class 'str'>
+type of (0, -1) is <class 'tuple'>
+type of [5, 12] is <class 'list'>
+type of {'class': 'V', 'section': 'A'} is <class 'dict'>
+"""
